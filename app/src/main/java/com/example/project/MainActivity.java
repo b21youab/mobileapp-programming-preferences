5384d8ecefef73adf8b2634f35764dc585bb0686
Youssuf <b21youab@his.student.se>
2023-05-03 18:36:32 +0200

    Modified MainActivity.java and mainActivity.xml and also added a secondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView output;
+    private Button button;
+    private final String key = String.valueOf(R.string.keyData);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        output = findViewById(R.id. mainButtonStartActivity);
+        button = findViewById(R.id. showOutput);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change();
+            }
+        });
     }
 
 }
