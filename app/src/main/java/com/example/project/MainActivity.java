cf9805dc2acc02e0a6c5ccbaac8b711dc4875e9d
Youssuf <b21youab@his.student.se>
2023-05-04 18:27:24 +0200

    Fixed few warnings in Main.java and also corrected few id:s and design options in main.xml.
    I also fixed a bug where the data from SecondActivity was never sent back to MainActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,22 +16,44 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView output;
     private Button button;
-    private final String key = String.valueOf(R.string.keyData);
+    private TextView sharedData;
+    private final String key = String.valueOf(R.string.inputData);
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedData = findViewById(R.id.nameTextView);
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
-        output = findViewById(R.id. mainButtonStartActivity);
-        button = findViewById(R.id. showOutput);
+        button = findViewById(R.id. ActivityStartButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 change();
             }
         });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume-b21youab", "Main activity active");
+        String defaultValue = "Share something.";
+         SharedPreferences sharedPref = getSharedPreferences(key, MODE_PRIVATE);
+        sharedData.setText(sharedPref.getString(key, defaultValue));
+    }
+
+
+
+
+    public void switchActivity() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,49 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+    private final String key = String.valueOf(R.string.inputData);
+    private Button button;
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        sharedPreferences = getSharedPreferences(key, MODE_PRIVATE);
+
+        button = findViewById(R.id.secondButtonsave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retainData();
+                finish();
+
+
+            }
+        });
     }
+
+
+    private void retainData() {
+        EditText userInputEditText = findViewById(R.id.showInput);
+        String userInputData = userInputEditText.getText().toString();
+
+    //    SharedPreferences sharedPreferences = getSharedPreferences("inputData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(key, userInputData);
+        String key = "sharedPreferences";
+
+        editor.apply();
+    }
+
 }
